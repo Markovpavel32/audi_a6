d83b09f5407cb1cee636eef7a236bbd172917d51
hunky <markovpavel32@gmail.com>
2019-07-09 19:01:27 +0300

    Добавлено фото авто, надпись в хэдер

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      msg: 'Специальные предложения на автомобили Audi A6 45 TFSI quattro'
+    }
+  }
+}
+</script>
